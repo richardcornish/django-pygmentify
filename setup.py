f07c379bb75d71ad7c4fb568e4b48b50abb34fdd
Richard Cornish <richardcornish@gmail.com>
2017-04-02 19:08:43 -0500

    Updated setup.py description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 setup(
     name='django-pygmentify',
     version='0.3.0',
-    description='A Django template filter application to highlight code with Pygments',
+    description='A Django template tag to highlight code with Pygments',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     author='Richard Cornish',
     author_email='rich@richardcornish.com',
